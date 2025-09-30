f55f380c05a629897620a3ce037f8e948073566c
tulioribeiro <tuliocribeiro@gmail.com>
2025-09-30 11:45:21 -0300

    feat: auth base layout and login form styled

--- web/src/app/layouts/auth.tsx
+++ web/src/app/layouts/auth.tsx
@@ -1,6 +1,8 @@
+import { Parentheses } from 'lucide-react'
 import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router'
 
+import background from '@/assets/auth-bg.jpg'
 import { useAuthGuard } from '@/features/auth/hooks/use-auth-guard'
 
 export function AuthLayout() {
@@ -21,8 +23,33 @@ export function AuthLayout() {
   }
 
   return (
-    <div>
-      <Outlet />
+    <div className="flex min-h-svh flex-col justify-center p-6 lg:grid lg:grid-cols-3 lg:p-0">
+      <div className="relative gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 lg:justify-start">
+          <img
+            src={background}
+            className="absolute inset-0 hidden h-full w-full object-cover opacity-5 grayscale lg:block"
+            alt=""
+          />
+          <div className="absolute inset-0 hidden bg-gradient-to-b from-black/50 to-black/30 lg:block" />
+
+          <h1 className="relative flex items-center gap-1 font-medium">
+            <span className="text-primary">
+              <Parentheses size={30} />
+            </span>
+            <span>
+              <span className="font-light">split</span>
+              <span className="dark:text-muted-foreground/70">/</span>
+              <span className="font-bold">equation</span>
+            </span>
+          </h1>
+        </div>
+      </div>
+      <div className="col-span-2 flex items-center justify-center">
+        <div className="w-full max-w-sm">
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
--- web/src/features/auth/pages/login.tsx
+++ web/src/features/auth/pages/login.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router'
+
 import { useLogin } from '@/features/auth/hooks/use-login'
 import { Button } from '@/shared/components/ui/button'
 import { Input } from '@/shared/components/ui/input'
@@ -20,35 +22,54 @@ function LoginPage() {
   }
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <div className="grid gap-2">
-          <h1 className="mb-4">Login Page</h1>
-          <div className="grid gap-1.5">
-            <Label htmlFor="email">Email:</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              required
-              value="admin@example.com"
-            />
-          </div>
-          <div className="grid gap-1.5">
-            <Label htmlFor="password">Password:</Label>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              required
-              value="hardpassword"
-            />
-          </div>
-
-          <Button type="submit">Login</Button>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h2 className="text-2xl font-bold">Log in to your account</h2>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your username and password to access your account.
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            name="email"
+            id="email"
+            type="email"
+            placeholder="email@example.com"
+            value="admin@example.com"
+            required
+          />
         </div>
-      </form>
-    </main>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link
+              to="/auth/forgot-password"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            value="hardpassword"
+            required
+          />
+        </div>
+        <Button type="submit" className="w-full">
+          Log in
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Don't have an account?{' '}
+        <Link to="/auth/signup" className="underline underline-offset-4">
+          Sign up
+        </Link>
+      </div>
+    </form>
   )
 }
 
